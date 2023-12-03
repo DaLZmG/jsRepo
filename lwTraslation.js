491085ff088cb96977c20d23b435a211c8346146
DaLZmG <jfgm007@gmail.com>
2023-12-04 00:56:44 +0100

    Update lwTraslation.js

--- lwTraslation.js
+++ lwTraslation.js
@@ -4,7 +4,7 @@ window.onload = ((event) => {
     let menuElements = document.getElementsByClassName('lw-topbar-option-link-lbl');
     menuElements.forEach((el) => { 
       if (el.innerText == 'Comunidad') { 
-        el.innerText = 'Community';
+        el.innerText = 'Comunidad';
         console.log('<<<< Done!!! >>>>>');
       }
     });
