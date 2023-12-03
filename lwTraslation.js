cd0a03e06574553ea27bb9870e3d7eb7ccc6d3c4
DaLZmG <jfgm007@gmail.com>
2023-12-04 00:41:13 +0100

    Update lwTraslation.js

--- lwTraslation.js
+++ lwTraslation.js
@@ -1,5 +1,5 @@
 
-window.addEventListener("load", (event) => {
+window.onload = (event) => {
   let menuElements = document.getElementsByClassName('lw-topbar-option-link-lbl');
   menuElements.forEach((el) => { 
     // console.log(el.innerText);
