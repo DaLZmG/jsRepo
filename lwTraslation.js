84a50009b1a2098a468e2fdbadbcc87d3556e842
DaLZmG <jfgm007@gmail.com>
2024-04-03 11:14:34 +0200

    Update lwTraslation.js

--- lwTraslation.js
+++ lwTraslation.js
@@ -4,7 +4,7 @@ window.onload = ((event) => {
     let menuElements = document.getElementsByClassName('lw-topbar-option-link-lbl');
     menuElements.forEach((el) => { 
       if (el.innerText == 'Comunidad') { 
-        el.innerText = 'Tomás';
+        el.innerText = 'Community';
         console.log('<<<< Done!!! >>>>>');
       } else if (el.innerText == 'Mis Cursos') {
         el.innerText = 'Mis Cursos';
